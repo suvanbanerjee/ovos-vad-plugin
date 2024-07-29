de0790850e845f0a4d466978c79facd17942bea6
suvanbanerjee <banerjeesuvan@gmail.com>
2024-07-29 23:42:40 +0530

    code dump

--- ovos_vad_plugin_noise/silence.py
+++ ovos_vad_plugin_noise/silence.py
@@ -1,148 +1,113 @@
-# Copyright 2022 Mycroft AI Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import audioop
-import typing
-from enum import Enum
+"""
+(c)2024 Suvan Banerjee
+This code is licensed under MIT license (see LICENSE for details)
+
+Simple energy-based voice activity detector.
+Thanks to Michael Hansen (@rhasspy) for the original implementation.
+check the original code here: https://github.com/rhasspy/energy-vad
+"""
+
+import array
+import math
+import statistics
+from typing import List, Optional
+
+_SAMPLE_RATE = 16000
+_SAMPLE_WIDTH = 2  # 16-bit
 
 
-class SilenceMethod(str, Enum):
-    """Method used to determine if an audio frame contains silence.
-
-    Values
-    ------
-    RATIO
-      Only use max/current energy ratio threshold
-
-    THRESHOLD
-      Only use current energy threshold
-
-    ALL
-      max/current energy ratio, and current energy threshold
-    """
-    RATIO = "ratio"
-    THRESHOLD = "threshold"
-    ALL = "all"
-
-
-class SilenceDetector:
-    """Detect speech/silence using noise threshold
-
-    Attributes
-    ----------
-    max_energy: Optional[float] = None
-        Maximum denoise energy value (None for dynamic setting from observed audio)
-
-    max_current_ratio_threshold: Optional[float] = None
-        Ratio of max/current energy below which audio is considered speech
-
-    current_energy_threshold: Optional[float] = None
-        Energy threshold above which audio is considered speech
-
-    silence_method: SilenceMethod = "all"
-        Method for deciding if an audio chunk contains silence or speech
-    """
+class EnergyVad:
+    """Energy-based voice activity detector (VAD)."""
 
     def __init__(
-            self,
-            max_energy: typing.Optional[float] = None,
-            max_current_ratio_threshold: typing.Optional[float] = None,
-            current_energy_threshold: typing.Optional[float] = None,
-            silence_method: SilenceMethod = SilenceMethod.ALL
-    ):
-        self.energy = 0
+        self,
+        threshold: Optional[float] = None,
+        samples_per_chunk: int = 240,
+        calibrate_seconds: float = 0.5,
+        calibrate_zscore_threshold: float = 1.0,
+    ) -> None:
+        """
+        Initialize VAD.
 
-        self.max_energy = max_energy
-        self.dynamic_max_energy = max_energy is None
-        self.dynamic_thresh = current_energy_threshold is None
-        self.max_current_ratio_threshold = max_current_ratio_threshold
-        self.current_energy_threshold = current_energy_threshold
-        self.silence_method = silence_method
+        Parameters
+        ----------
+        threshold: float, optional
+            Energy threshold, above which is considered speech.
+            If None, it will be set during calibration.
+        samples_per_chunk: int
+            Number of samples that process_chunk will expect.
+        calibrate_seconds: float
+            Seconds of audio that will be used to calibrate threshold.
+        calibrate_zscore_threshold: float
+            Only energies below this (median) z-score threshold will be used in
+            calibration.
+        """
+        self.samples_per_chunk = samples_per_chunk
+        self.bytes_per_chunk = self.samples_per_chunk * _SAMPLE_WIDTH
+        self.seconds_per_chunk = self.samples_per_chunk / _SAMPLE_RATE
 
-        if self.silence_method in [
-            SilenceMethod.RATIO,
-            SilenceMethod.ALL,
-        ]:
-            self.use_ratio = True
-            assert (
-                    self.max_current_ratio_threshold is not None
-            ), "Max/current ratio threshold is required"
-        else:
-            self.use_ratio = False
+        self.threshold = threshold
 
-        if self.silence_method in [
-            SilenceMethod.THRESHOLD,
-            SilenceMethod.ALL,
-        ]:
-            self.use_current = True
-        else:
-            self.use_current = False
+        self.calibrate_seconds = calibrate_seconds
+        self.calibrate_zscore_threshold = calibrate_zscore_threshold
+        self._calibrate_seconds_left = self.calibrate_seconds
+        self._calibrate_energies: List[float] = []
 
-        if not self.use_current and not self.use_ratio:
-            self.use_ratio = True
+    def reset_calibration(self) -> None:
+        """Reset threshold and calibration time."""
+        self._calibrate_seconds_left = self.calibrate_seconds
+        self._calibrate_energies.clear()
+        self.threshold = None
 
-    def reset(self):
-        self.energy = 0
-        if self.dynamic_thresh and self.max_energy:
-            self.current_energy_threshold = self.max_energy * 0.3
-        if self.dynamic_max_energy:
-            self.max_energy = None
+    def process_chunk(self, chunk: bytes) -> Optional[bool]:
+        """
+        Process a single chunk of audio.
 
-    def is_silence(self, chunk: bytes, energy: typing.Optional[float] = None) -> bool:
-        """True if audio chunk contains silence."""
-        all_silence = True
+        Parameters
+        ----------
+        chunk: bytes
+            Chunk of 16-bit mono PCM audio at 16Khz.
+            Must be exactly self.bytes_per_chunk in size.
 
-        if self.use_ratio or self.use_current:
-            # Compute debiased energy of audio chunk
-            energy = SilenceDetector.get_debiased_energy(chunk)
+        Returns
+        -------
+        bool, optional
+            None if chunk was used for calibration.
+            True if energy > threshold, False otherwise.
+        """
+        if len(chunk) != self.bytes_per_chunk:
+            raise ValueError(f"Chunk must be {self.bytes_per_chunk} bytes")
 
-            if self.use_ratio:
-                # Ratio of max/current energy compared to threshold
-                if self.dynamic_max_energy:
-                    # Overwrite max energy
-                    if self.max_energy is None:
-                        self.max_energy = energy
-                    else:
-                        self.max_energy = max(energy, self.max_energy)
+        chunk_array = array.array("h", chunk)
 
-                assert self.max_energy is not None
-                if energy > 0:
-                    ratio = self.max_energy / energy
-                else:
-                    # Not sure what to do here
-                    ratio = 0
-
-                assert self.max_current_ratio_threshold is not None
-                all_silence = all_silence and (ratio > self.max_current_ratio_threshold)
-            elif self.use_current and self.current_energy_threshold is not None:
-                # Current energy compared to threshold
-                all_silence = all_silence and (energy < self.current_energy_threshold)
-
-        if all_silence or not self.energy:
-            self.energy = energy
-        return all_silence
-
-    @staticmethod
-    def get_debiased_energy(audio_data: bytes) -> float:
-        """Compute RMS of debiased audio."""
-        # Thanks to the speech_recognition library!
-        # https://github.com/Uberi/speech_recognition/blob/master/speech_recognition/__init__.py
-        energy = -audioop.rms(audio_data, 2)
-        energy_bytes = bytes([energy & 0xFF, (energy >> 8) & 0xFF])
-        debiased_energy = audioop.rms(
-            audioop.add(audio_data, energy_bytes * (len(audio_data) // 2), 2), 2
+        # Compute RMS
+        energy = -math.sqrt(sum(x**2 for x in chunk_array) / len(chunk_array))
+        debiased_energy = math.sqrt(
+            sum((x + energy) ** 2 for x in chunk_array) / len(chunk_array)
         )
 
-        # Probably actually audio if > 30
-        return debiased_energy
+        if self.threshold is None:
+            if self._calibrate_seconds_left <= 0:
+                # Enough energy values are available for calibration.
+                # Calculate median z-score to remove high-energy clicks.
+                median = statistics.median(self._calibrate_energies)
+                stdev = statistics.stdev(self._calibrate_energies)
+                z_score = [(x - median) / stdev for x in self._calibrate_energies]
+
+                # Filter outliers, but fall back to using all energies if
+                # everything gets filtered out.
+                energies = [
+                    x
+                    for i, x in enumerate(self._calibrate_energies)
+                    if z_score[i] < self.calibrate_zscore_threshold
+                ] or self._calibrate_energies
+
+                self.threshold = statistics.mean(energies) + statistics.stdev(energies)
+            else:
+                self._calibrate_energies.append(debiased_energy)
+                self._calibrate_seconds_left -= self.seconds_per_chunk
+
+            # Calibrating
+            return None
+
+        return debiased_energy > self.threshold
